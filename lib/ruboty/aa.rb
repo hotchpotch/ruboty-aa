8ba2a862322679a8e97ca2d530700c280f72a23e
Yuichi Tateno <hotchpotch@gmail.com>
2014-09-03 16:24:00 +0900

    Init

---
+++ lib/ruboty/aa.rb
@@ -0,0 +1,34 @@
+require "ruboty/aa/version"
+require "artii"
+
+module Ruboty
+  module Handlers
+    class Aa < Base
+      on(
+        /aa(?:\s+(.*))?/,
+        name: 'aa',
+        description: 'Ascii Art'
+      )
+
+      def prefix
+        ENV['AA_PREFIX'] ||= "```\n"
+      end
+
+      def suffix
+        ENV['AA_SUFFIX'] ||= "\n```"
+      end
+
+
+      def artii
+        @artii ||= Artii::Base.new(font: (ENV['AA_FONT'] || 'big'))
+      end
+
+      def aa(message)
+        word = message.match_data[1]
+        if word && !word.empty?
+          message.reply prefix + artii.asciify(word) + suffix
+        end
+      end
+    end
+  end
+end
---
+++ lib/ruboty/aa/version.rb
@@ -0,0 +1,5 @@
+module Ruboty
+  module Aa
+    VERSION = "0.0.1"
+  end
+end
